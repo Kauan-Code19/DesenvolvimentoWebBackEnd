41b7fdcf4f983927df9fff9482c35556abd28243
Kauan-Code19 <pkauprofissional@gmail.com>
2024-05-22 17:13:09 -0300

    import nao utilizado

--- src/main/java/com/kauan/DesenvolvimentoWeb/dtos/user/UserDTO.java
+++ src/main/java/com/kauan/DesenvolvimentoWeb/dtos/user/UserDTO.java
@@ -1,6 +1,5 @@
 package com.kauan.DesenvolvimentoWeb.dtos.user;
 
-import com.kauan.DesenvolvimentoWeb.entities.user.UserEntity;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
